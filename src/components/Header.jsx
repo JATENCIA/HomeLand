1127d06eecef0eda4a9c71ff5f80f0b84109f9d8
JATENCIA <atenciajaramilloj@gmail.com>
2023-07-24 14:21:49 -0500

    Finished

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom'
+import Logo from '../assets/img/logo.svg'
+
+const Header = () => {
+  return (
+    <header className='py-6 mb-12 border-b'>
+      <div className='container max-auto flex justify-between items-center'>
+        <Link to='/'>
+          <img src={Logo} alt='Logo website' />
+        </Link>
+        <div className='flex items-center gap-6'>
+          <Link className='hover:text-violet-900 transition' to=''>
+            Log in
+          </Link>
+          <Link
+            className='bg-violet-700 hover:bg-violet-800 text-white px-4 py-3 rounded-lg transition'
+            to=''>
+            Sing up
+          </Link>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
